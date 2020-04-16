0ad7d52148f38ea4c5ddb8b1737a888f6e828b8e
John McMillan <john@mcmillantech.co.uk>
2020-04-16 15:26:56 +0100

    16/04/20 - Shows all after build

--- admin/AjaxLogOn.php
+++ admin/AjaxLogOn.php
@@ -1,49 +1,47 @@
 <?php
 // -------------------------------------------------------------
-//  Project	Mells Roofing
-//	File	AjaxLogOn.php
+//  Project	Online Gallery
+//  File	AjaxLogOn.php
 //
-//	Author	John McMillan
+//  Author	John McMillan
 //  McMillan Technology Ltd
 //
 //  Ajax server for log on
 //
-//	Checks the password
+//  Checks the password
 // --------------------------------------------------------------
-	session_start();
+    session_start();
 
-	global $name;
-	checkPassword();
+    global $name;
+    checkPassword();
 
 // -------------------------------------------
-//	Check the password
-//	If OK, log on
+// Check the password
+//  If OK, log on
 //
 // -------------------------------------------
 function checkPassword()
 {
-	$pw = $_GET['pw'];
-	if ($pw == "small-garden")
+    $pw = $_GET['pw'];
+    if ($pw == "small-garden")
 	{
-		logOn();
-		$_SESSION['userLevel'] = 1;
+            logOn();
+            $_SESSION['userLevel'] = 1;
 	}
 	else
-		echo "Error: wrong password";
+            echo "Error: wrong password";
 }
 
 // -------------------------------------------
-//	Log the user on
-//
+//  Log the user on
 //	
 // -------------------------------------------
 function logOn()
 {
-	$tm = time();
-	$_SESSION['MLastAccess'] = $tm;		// Initialise the time out
+    $tm = time();
+    $_SESSION['MLastAccess'] = $tm;		// Initialise the time out
 
-	echo 'OK';
+    echo 'OK';
 }
-
 
 ?>
