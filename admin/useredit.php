90dd530b75802ba7b40ac130b1fb274b02883d3f
John McMillan <john@mcmillantech.co.uk>
2020-04-20 14:59:24 +0100

    First draft of user table

---
+++ admin/useredit.php
@@ -0,0 +1,155 @@
+<?php
+// ------------------------------------------------------
+//  Project	OnLine Gallery
+//  File	useredit.php
+//		Edit user table
+//
+//  Parameters	
+//		mode - ins or upd
+//		id - index of item, upd mode only
+//
+//  Author	John McMillan, McMillan Technolo0gy
+// ------------------------------------------------------
+/*
+protected function setNewItem
+protected function fetchItem()
+protected function dispDate($dt)
+protected function showForm($mode)
+
+JS function showFileFrame()
+*/
+?>
+<!DOCTYPE html>
+
+<html lang="en-GB">
+<head>
+<meta http-equiv="Content-Language" content="en-gb">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Edit Gallery Users</title>
+<link type="text/css" rel="stylesheet" href="../Gallery.css">
+<link type="text/css" rel="stylesheet" href="../custom.css">
+<script src="../Cunha.js"></script>
+<style>
+.input
+{
+	position:			absolute;
+	left:				200px;
+}
+</style>
+<script>
+function showFileFrame()
+{
+	var el = document.getElementById("frame");
+	el.style.visibility = "visible";
+}
+
+</script>
+</head>
+
+<body>
+<?php
+    session_start();
+    require_once "../common.php";
+    require_once "DataEdit.php";
+
+class userEdit extends DataEdit
+{
+    function __construct($mysqli)
+    {
+        parent::__construct($mysqli);
+        $_SESSION['colEdit'] = 1;
+    }
+
+    protected function setNewItem()
+    {
+        $this->record = array
+        (
+            'username' => '',
+            'password' => '',
+            'fullname' => '',
+            'firstname' => '',
+            'email' => '',
+            'addr1' => '',
+            'addr2' => '',
+            'addr3' => '',
+            'addr4' => '',
+            'postcode' => '',
+            'website' => '',
+            'level' => ''
+        );
+    }
+
+// -------------------------------------------
+//	Override of DataEdit virtual function
+//
+//	Reads event into class member record
+// -------------------------------------------
+    protected function fetchItem()
+    {
+        $id = $_GET['item'];
+        $sql = "SELECT * FROM users WHERE id=$id";
+        $result = $this->mysqli->query($sql)
+            or die ("Error fetching item" . mysqli_error($this->mysqli));
+        $this->record =  mysqli_fetch_array($result, MYSQLI_ASSOC);
+    }
+
+// -------------------------------------------
+//	Show the event edit form
+//
+// -------------------------------------------
+    protected function showForm($mode)
+    {
+        if ($mode=='ins')
+            $action = "userlist.php?mode=ins";
+        else {
+            $id = $_GET['item'];
+            $action = "userlist.php?mode=upd&item=$id";
+        }
+
+        $dta = $this->record;
+
+        echo "<form method='post' action='$action'>";
+            $this->showLine('User Name', $dta, 'username', 16);
+            $this->showLine('Password', $dta, 'password', 16);
+            $this->showLine('First Name', $dta, 'firstname', 45);
+            $this->showLine('Full Name', $dta, 'fullname', 45);
+            $this->showLine('Email', $dta, 'email', 45);
+            $this->showLine('Address', $dta, 'addr1', 45);
+            $this->showLine('', $dta, 'addr2', 45);
+            $this->showLine('', $dta, 'addr3', 45);
+            $this->showLine('', $dta, 'addr4', 45);
+            $this->showLine('Postcode', $dta, 'postcode', 16);
+            $this->showLine('Website', $dta, 'website', 45);
+            $this->showLine('Level', $dta, 'level', 2);
+            echo "<button type='submit'>Post</button>";
+        echo "</form>";
+
+    }
+
+    private function userList()
+    {
+        $ar = array();
+        array_push($ar, '');
+
+        $sql = "SELECT * FROM users";
+        $result = $this->mysqli->query($sql)
+            or die ("Error reading link" . mysqli_error($this->mysqli));
+        while ($coll = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            array_push($ar, $coll['name']);
+        }
+
+        mysqli_free_result($result);
+        return $ar;
+    }
+}
+
+    $config = setConfig();					// Connect to database
+    $mysqli = dbConnect($config);
+
+    echo "<h3>Edit Users</h3>";
+    $edit = new userEdit($mysqli);
+    $edit->run();
+?>
+</body>
+</html>
+
---
+++ admin/userlist.php
@@ -0,0 +1,175 @@
+<?php
+// ------------------------------------------------------
+//  Project	OnLine Gallery
+//  File	userlist.php
+//		List users
+//
+//  Parameters	default - show list
+//		mode - ins or upd
+//
+//  Author	John McMillan, McMillan Technolo0gy
+// ------------------------------------------------------
+/*
+public function showListLine($line)
+public function showHeading()
+protected function insertItem()
+protected function upDateItem()
+protected function deleteItem()
+protected function SQLDate($dt)
+*/
+?>
+<!DOCTYPE html>
+
+<html lang="en-GB">
+<head>
+<meta http-equiv="Content-Language" content="en-gb">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Gallery Users</title>
+<link type="text/css" rel="stylesheet" href="../Gallery.css">
+<link type="text/css" rel="stylesheet" href="../custom.css">
+<script src="../Cunha.js"></script>
+<style>
+.lsUser
+{
+	position:		absolute;
+	left:			20px;
+	width:			320px;
+}
+
+.lsName
+{
+	position:		absolute;
+	left:			240px;
+}
+
+.lsLevel
+{
+	position:		absolute;
+	left:			550px;
+    
+}
+
+.lsButton
+{
+	position:		absolute;
+	left:			620px;
+}
+
+</style>
+</head>
+<body>
+<?php
+	session_start();
+//	include "adminmenus.php";
+	require_once "../common.php";
+	require_once "DataList.php";
+
+class UserList extends DataList
+{
+    function __construct($mysqli)
+    {
+        parent::__construct($mysqli);
+    }
+
+    public function showListLine($line)
+    {
+        $id = $line['id'];
+        $onEdit = "window.location=\"useredit.php?mode=upd&item=$id\"";
+        $onDelete = "window.location=\"userlist.php?mode=del&item=$id\"";
+
+        echo "\n<span class='lsUser'>" . $line['username'] . "</span>";
+        echo "\n<span class='lsName'>" . $line['fullname'] . "</span>";
+        echo "\n<span class='lsLevel'>" . $line['level'] . "</span>";
+        echo "<span class='lsButton'>";
+                echo "<button onClick='$onEdit'>Edit</button>";
+                echo "&nbsp;";
+                echo "<button onClick='$onDelete'>Delete</button>";
+        echo "</span><br>";
+    }
+
+    public function showHeading()
+    {
+        echo "<h2>Users</h2>";
+        echo "<span class='lsUser'>User</span>";
+        echo "<span class='lsName'>Name</span>";
+        echo "<span class='lsLevel'>Level</span>";
+        echo "<span class='lsButton'> Edit</span>";
+        echo "</b><br><br>";
+    }
+
+    // -------------------------------------------
+    //	Insert new item
+    //
+    // -------------------------------------------
+    protected function insertItem()
+    {
+        $sql = "INSERT INTO users "
+            . "(username, password, fullname, firstname, email, addr1, "
+            . "addr2, addr3, addr4, postcode, website, level ) "
+            . " VALUES ("
+            . $this->postField('username') . ','
+            . $this->postField('password') . ','
+            . $this->postField('fullname') . ','
+            . $this->postField('firstname') . ','
+            . $this->postField('email') . ','
+            . $this->postField('addr1') . ','
+            . $this->postField('addr2') . ','
+            . $this->postField('addr3') . ','
+            . $this->postField('addr4') . ','
+            . $this->postField('postcode') . ','
+            . $this->postField('website') . ','
+            . $this->postField('level') 
+            . ")";
+        $this->mysqli->query($sql)
+            or die ("Error inserting item " . mysqli_error($this->mysqli));
+    }
+
+    protected function upDateItem()
+    {
+        $id = $_GET['item'];
+
+        $sql = "UPDATE users SET "
+            . " username=" . $this->postField('username')
+            . " ,password=" . $this->postField('password')
+            . " ,fullname=" . $this->postField('fullname')
+            . " ,firstname=" . $this->postField('firstname')
+            . " ,email=" . $this->postField('email')
+            . " ,addr1=" . $this->postField('addr1')
+            . " ,addr2=" . $this->postField('addr2')
+            . " ,addr3=" . $this->postField('addr3')
+            . " ,addr4=" . $this->postField('addr4')
+            . " ,postcode=" . $this->postField('postcode')
+            . " ,website=" . $this->postField('website')
+            . " ,level=" . $_POST['level']
+            . " WHERE id=$id";
+
+        $this->mysqli->query($sql)
+            or die ("Error updating item " . mysqli_error($this->mysqli));
+    }
+
+    protected function deleteItem()
+    {
+        $id = $_GET['item'];
+        $sql = "DELETE FROM users WHERE id=$id";
+
+        $this->mysqli->query($sql)
+            or die ("Error deleting item " . mysqli_error($this->mysqli));
+    }
+
+}
+
+// -------------------------------------------
+//  Entry point to page
+// -------------------------------------------
+
+    $config = setConfig();			// Connect to database
+    $mysqli = dbConnect($config);
+
+    echo "<h3>Users</h3>";
+    $lst = new UserList($mysqli);
+    $lst->sqlShow("SELECT * FROM users");
+    $lst->editPage("useredit.php");
+    $lst->run(); 
+?>
+</body>
+</html>
