c0361580136f4ec04ca82f3dd991d66a7f225603
John McMillan <john@mcmillantech.co.uk>
2020-05-25 12:05:46 +0100

    Temporary change to inhibit putchasing

--- order.php
+++ order.php
@@ -44,6 +44,10 @@ JS function checkFld(fld)
     $title = $record['name'];
     showTop("Order Cunha Painting", "Order $title");
 
+    $holdText = "This site will be live from June 1st 2020. Until then, it "
+            . "is not possible to make purchases. Please return after June 1st.";
+    echo $holdText;
+    exit();
     showForm($id, $record);
 	
 // ---------------------------------------
