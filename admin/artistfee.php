1dea95b9fbc67babd783c509c2b6c2f14eaf61f1
John McMillan <john@mcmillantech.co.uk>
2020-05-29 17:38:15 +0100

    Initial version

---
+++ admin/artistfee.php
@@ -0,0 +1,108 @@
+<?php
+// ------------------------------------------------------
+//  Project	OnLine Gallery Admin
+//  File	admin/artistfee.php
+//  		Take payment from artist
+//
+//  Author	John McMillan, McMillan Technolo0gy
+// ------------------------------------------------------
+    session_start();
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+    require_once "../common.php";
+
+    $config = setConfig();					// Connect to database
+    $mysqli = dbConnect($config);
+	
+?>
+<!DOCTYPE html>
+
+<html lang="en-GB">
+<head>
+<meta http-equiv="Content-Language" content="en-gb">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>New Art for You Join</title>
+<link type="text/css" rel="stylesheet" href="../Gallery.css">
+<script src="../Cunha.js"></script>
+<script src="https://js.braintreegateway.com/web/dropin/1.11.0/js/dropin.min.js">
+</script>
+
+</head>
+<body>
+<?php
+    logCheck();
+
+    echo "<h3>Artist fee</h3>";
+    echo "<p>Please give your card details to activate your account/p>";
+
+?>
+    
+    <div id='dropin-container' style='width:600px; margin-left:50px;'></div>
+        <button id="submit-button">Pay now</button>
+    <form id="checkout" method="post" action="artistpaid.php">
+        <input type='hidden' value='testnonce' name='nonce' id='nonceFld'>
+
+    <br>
+    </form>
+
+<script>
+    var domain = document.domain;
+    var token;
+    token = "sandbox_79k7qx4p_nymy4h8qq7ck73sn";
+
+// ---------------------------------------------------
+//	Script taken from Braintree
+//
+// ---------------------------------------------------
+    var button = document.querySelector('#submit-button');
+    var form = document.querySelector('#checkout');
+    var nonce = document.querySelector('#nonceFld');
+
+    var result = braintree.dropin.create(
+    {
+		authorization: token,
+      container: '#dropin-container'
+    }, function (createErr, instance) 
+     {
+     	button.addEventListener('click', 
+      	  function () 
+      	{
+        	instance.requestPaymentMethod(function (err, payload) 
+        	{
+          // Submit payload.nonce to your server
+          nonce.value = payload.nonce;
+          form.submit();
+        	});
+        });
+     }
+     );
+</script>
+</body>
+</html>
+<?php
+    
+function logCheck()
+{
+                            // See if a user is logged on
+    if (!array_key_exists('userLevel', $_SESSION)) {
+        echo "<button onClick='logon(\"artistfee.php\")'>Please log on</button> ";
+        die();
+    }
+                            // Now there should be a session - process timeout
+    $tm = time();
+    $lastTime = 0;
+    if (array_key_exists('MLastAccess', $_SESSION))
+        $lastTime = $_SESSION['MLastAccess'];
+    if (($tm - $lastTime) > 3600) {
+        session_unset();
+        session_destroy();
+        header('Location: logon.php?page=artistfee.php');
+    }
+
+    $_SESSION['MLastAccess'] = $tm;
+
+}
+
+?>
+
+
---
+++ admin/artistpaid.php
@@ -0,0 +1,216 @@
+<?php
+// ------------------------------------------------------
+//  Project	Artist Gallery
+//  File	artistpaid.php
+//		Return from Braintree
+//
+//  Author	John McMillan, McMillan Technology
+// ------------------------------------------------------
+
+    session_start();
+    ini_set("display_errors", "1");
+    error_reporting(E_ALL);
+    require '../common.php';
+    $config = setConfig();			// Connect to database
+    $mysqli = dbConnect($config);
+
+                                    // Prepend parent dir to Btaintree path
+                                    // (Config holds relative to server root)
+    $config['braintree'] = "../" . $config['braintree'];
+
+    require_once "../bootstrap.php";
+
+    const TEST = 0;				// Set to 1 to skip emails
+    const RERUN = 0;				// Set to 1 to skip Braintree
+
+    $dbConnection = dbConnect($config);
+
+    $gateway = new Braintree_Gateway(
+        [
+            'environment' => $environment,
+            'merchantId' => $merchantId,
+            'publicKey' => $publicKey,
+            'privateKey' => $privateKey
+        ]
+    );
+
+    $userName = userName();
+    $ref = getNextOrder();
+    
+    if (RERUN) {
+        doSuccess('981j6c7k');
+        return;
+    }
+    else if (RERUN == 0) 
+        takePayment($gateway);
+
+    echo "To exit, close this browser window";
+//    echo "<br><button onClick='window.location.assign(\"index.php\")'>Done</button>";
+
+// ----------------------------------------
+//	Process Braintree payment
+//	and notify customer
+//
+// ----------------------------------------
+function takePayment($gateway)
+{				// Instantiate a Braintree Gateway 
+    global $ref;
+
+    $nonceFromTheClient = $_POST["nonce"];
+                                                // Then, create a transaction:
+    $result = $gateway->transaction()->sale([
+        'amount' => 25.00,
+        'paymentMethodNonce' => "$nonceFromTheClient",
+        'options' => [ 'submitForSettlement' => true]
+    ]);
+    if ($result->success) {
+        $transaction = $result->transaction;
+        $transRef = $transaction->id;
+        doSuccess($transRef);
+    } else if ($result->transaction) {
+        doFailure($result->transaction->processorResponseText);
+    } else {
+        $msg = $result->message;
+        doFailure($msg);
+    }
+}
+
+// ----------------------------------------
+//	Payment failure
+//
+//	Notify customer
+// ----------------------------------------
+function doFailure($msg)
+{
+
+    echo "We are sorry, payment has not been accepted:<br>";
+    echo "The message was $msg<br><br>";
+}
+
+// -----------------------------------
+//	Process successful receipt
+//
+// -----------------------------------
+function doSuccess($transRef)
+{
+    global $config, $dbConnection, $userName;
+
+                                        // Present on screen message to artist
+    echo "Thank you. Your payment has been received.<br><br>";
+
+    updateUser();
+    writeOrder($transRef);
+    supportEmail($userName, $transRef);
+}
+
+function writeOrder($transRef)
+{
+    global $mysqli, $ref, $userName;
+    
+    $user = $_SESSION['userId'];
+    $dtSQL = date('Y-m-d');		// Makes today's date for SQL insertion
+    $sql = "INSERT INTO orders (ref, name, user, addr1, postcode, status, "
+            . "price, date, transref) "
+        . "VALUES ($ref, '$userName', $user, 'artist', 'artist', 1, 2500, "
+        . "'$dtSQL', '$transRef')";
+    $mysqli->query($sql)
+        or die ("Error writing order $sql");
+    
+}
+
+// ----------------------------------------
+//  send email to site owner
+//
+//  Parameters	Artist name
+// ----------------------------------------
+function supportEmail($userName, $transRef)
+{
+    global $ref;
+    
+    $from = ARTIST;
+    $replyTo = USER_EMAIL;
+    $to = USER_EMAIL;
+
+    $subject = 'Payment received from artist';
+    $headers = "From: " . USER_EMAIL . "\r\n" .
+        "Reply-To: " . USER_EMAIL . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= "From: $from\r\n"
+        . "Reply-To: $replyTo\r\n" 
+        . "X-Mailer: PHP/" . phpversion();
+
+    $message = "Hi\r\n\r\n"
+        . "Payment has been received from $userName. "
+        . "\r\nOrder reference is $ref"
+        . "\r\nTransaction is $transRef";
+
+    if (TEST == 0) {
+        $reply = mail($to, $subject, $message, $headers);
+        if (!$reply)
+            myError(ERR_PM_CUSTOMER_EMAIL, "Email failed to send to $to<br>");
+        echo "Mail sent to admin";
+    }
+    else {
+        echo $message;
+        echo "---- Customer mail -----";
+    }
+
+}
+
+// ----------------------------------------------
+//  Update the user
+//
+// ----------------------------------------------
+function updateUser()
+{
+    global $mysqli;
+
+    $userId = $_SESSION['userId'];
+    $status = 4;
+    
+    $sql = "UPDATE users SET status=4 WHERE id=$userId";
+    $errTxt = "There was a problem updating your record: $sql "
+        . "Please pass this message to admin";
+    $mysqli->query ($sql)
+            or die($errTxt);
+}
+
+function userName()
+{
+    global $mysqli;
+
+    $userId = $_SESSION['userId'];
+    $sql = "SELECT fullname FROM users where id = $userId";
+    $result = $mysqli->query($sql);
+    if ($result=== FALSE)
+        echo "There has been an error: $sql. Please pass this message to admin";
+    $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    
+    return $record['fullname'];
+}
+
+// ----------------------------------------------
+//	Generate next order number from system table
+//
+// ----------------------------------------------
+function getNextOrder()
+{
+	global $mysqli;
+
+	$result = $mysqli->query("SELECT * FROM system")
+		or die ("System read error");
+	$record = $result->fetch_array(MYSQLI_ASSOC);
+	$ordId = $record['nextorder'] + 1;
+	$sql = "UPDATE system SET nextorder = $ordId";
+	$mysqli->query($sql);
+
+	return $ordId;
+}
+
+?>
+</body>
+</html>
+
